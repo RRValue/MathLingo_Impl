a7fd8c3e21faaa0b90b2e35baacca68df56b50c0
RRValue <rrvalue@codeschnipsel.org>
2024-02-05 12:19:22 +0100

    add test to evaluate one plus two

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn simpple_addition() {
+        let input = quote!(one plus two);
+        let output = evaluate_math_lingo(input);
+        let expected_output = quote!(3);
+
+        assert_tokenstreams_eq!(&output, &expected_output);
     }
 }
