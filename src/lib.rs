1673b9295bd787550ffd3ce591ce832c48032073
RRValue <rrvalue@codeschnipsel.org>
2024-02-05 12:48:47 +0100

    add add all signle digit number keywords, enum for all 10 single digit numbers

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,45 @@ use syn::parse::{Parse, ParseStream};
 use syn::Result;
 
 mod kw {
+    syn::custom_keyword!(zero);
     syn::custom_keyword!(one);
     syn::custom_keyword!(two);
+    syn::custom_keyword!(three);
+    syn::custom_keyword!(four);
+    syn::custom_keyword!(five);
+    syn::custom_keyword!(six);
+    syn::custom_keyword!(seven);
+    syn::custom_keyword!(eight);
+    syn::custom_keyword!(nine);
+    
     syn::custom_keyword!(plus);
 }
 
+enum Number {
+    Zero(kw::zero),
+    One(kw::one),
+    Two(kw::two),
+    Three(kw::three),
+    Four(kw::four),
+    Five(kw::five),
+    Six(kw::six),
+    Seven(kw::seven),
+    Eight(kw::eight),
+    Nine(kw::nine),
+}
+
 struct BasicSentence {
-    pub one: kw::one,
+    pub numer_left: Number,
     pub plus: kw::plus,
-    pub two: kw::two,
+    pub numer_right: Number,
 }
 
 impl Parse for BasicSentence {
     fn parse(input: ParseStream) -> Result<Self> {
-        let one: kw::one = input.parse()?;
+        let numer_left: Number = input.parse()?;
         let plus: kw::plus = input.parse()?;
-        let two: kw::two = input.parse()?;
-        Ok(BasicSentence{one, plus, two})
+        let numer_right: Number = input.parse()?;
+        Ok(BasicSentence{numer_left, plus, numer_right})
     }
 }
 
