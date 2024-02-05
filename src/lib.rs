fc167e292ada32d3c618f36cc9d47e8e00328d7e
RRValue <rrvalue@codeschnipsel.org>
2024-02-05 16:27:09 +0100

    allow usual numbers like 1, -2

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream;
 use syn::parse::{Parse, ParseStream};
-use syn::Result;
-use quote::quote;
+use syn::{LitInt, Result};
+use quote::{quote, ToTokens};
 
 mod kw {
     syn::custom_keyword!(zero);
@@ -34,46 +34,6 @@ enum Number {
     Nine,
 }
 
-impl Parse for Number {
-    fn parse(input: ParseStream) -> Result<Self> {
-        let lookahead = input.lookahead1();
-
-        if lookahead.peek(kw::zero) {
-            input.parse::<kw::zero>()?;
-            Ok(Number::Zero)
-        } else if lookahead.peek(kw::one) {
-            input.parse::<kw::one>()?;
-            Ok(Number::One)
-        } else if lookahead.peek(kw::two) {
-            input.parse::<kw::two>()?;
-            Ok(Number::Two)
-        } else if lookahead.peek(kw::three) {
-            input.parse::<kw::three>()?;
-            Ok(Number::Three)
-        } else if lookahead.peek(kw::four) {
-            input.parse::<kw::four>()?;
-            Ok(Number::Four)
-        } else if lookahead.peek(kw::five) {
-            input.parse::<kw::five>()?;
-            Ok(Number::Five)
-        } else if lookahead.peek(kw::six) {
-            input.parse::<kw::six>()?;
-            Ok(Number::Six)
-        } else if lookahead.peek(kw::seven) {
-            input.parse::<kw::seven>()?;
-            Ok(Number::Seven)
-        } else if lookahead.peek(kw::eight) {
-            input.parse::<kw::eight>()?;
-            Ok(Number::Eight)
-        } else if lookahead.peek(kw::nine) {
-            input.parse::<kw::nine>()?;
-            Ok(Number::Nine)
-        } else {
-            Err(lookahead.error())
-        }
-    }
-}
-
 impl Number {
     fn quote(&self) -> TokenStream{
         match self {
@@ -87,6 +47,62 @@ impl Number {
             Number::Seven => quote!(7),
             Number::Eight => quote!(8),
             Number::Nine => quote!(9),
+        }
+    }
+}
+
+enum NumberRep {
+    NumberStr(Number),
+    NumberLit(LitInt)
+}
+
+impl Parse for NumberRep {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+
+        if lookahead.peek(kw::zero) {
+            input.parse::<kw::zero>()?;
+            Ok(NumberRep::NumberStr(Number::Zero))
+        } else if lookahead.peek(kw::one) {
+            input.parse::<kw::one>()?;
+            Ok(NumberRep::NumberStr(Number::One))
+        } else if lookahead.peek(kw::two) {
+            input.parse::<kw::two>()?;
+            Ok(NumberRep::NumberStr(Number::Two))
+        } else if lookahead.peek(kw::three) {
+            input.parse::<kw::three>()?;
+            Ok(NumberRep::NumberStr(Number::Three))
+        } else if lookahead.peek(kw::four) {
+            input.parse::<kw::four>()?;
+            Ok(NumberRep::NumberStr(Number::Four))
+        } else if lookahead.peek(kw::five) {
+            input.parse::<kw::five>()?;
+            Ok(NumberRep::NumberStr(Number::Five))
+        } else if lookahead.peek(kw::six) {
+            input.parse::<kw::six>()?;
+            Ok(NumberRep::NumberStr(Number::Six))
+        } else if lookahead.peek(kw::seven) {
+            input.parse::<kw::seven>()?;
+            Ok(NumberRep::NumberStr(Number::Seven))
+        } else if lookahead.peek(kw::eight) {
+            input.parse::<kw::eight>()?;
+            Ok(NumberRep::NumberStr(Number::Eight))
+        } else if lookahead.peek(kw::nine) {
+            input.parse::<kw::nine>()?;
+            Ok(NumberRep::NumberStr(Number::Nine))
+        } else if lookahead.peek(LitInt) {
+            Ok(NumberRep::NumberLit(input.parse::<LitInt>()?))
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+impl NumberRep {
+    fn quote(&self) -> TokenStream{
+        match self {
+            NumberRep::NumberStr(value) => value.quote(),
+            NumberRep::NumberLit(lit) => lit.to_token_stream(),
         }
     }
 }
@@ -132,16 +148,16 @@ impl Operation {
 }
 
 struct BasicSentence {
-    pub number_left: Number,
+    pub number_left: NumberRep,
     pub operation: Operation,
-    pub number_right: Number,
+    pub number_right: NumberRep,
 }
 
 impl Parse for BasicSentence {
     fn parse(input: ParseStream) -> Result<Self> {
-        let number_left: Number = input.parse()?;
+        let number_left: NumberRep = input.parse()?;
         let operation: Operation = input.parse()?;
-        let number_right: Number = input.parse()?;
+        let number_right: NumberRep = input.parse()?;
         Ok(BasicSentence{number_left, operation, number_right})
     }
 }
@@ -164,6 +180,14 @@ pub fn evaluate_math_lingo(input: TokenStream) -> TokenStream{
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn simple_addition_literals() {        
+        let output = evaluate_math_lingo(quote!(1 plus 2));
+        let expected_output = quote!(1 + 2);
+        
+        assert_eq!(output.to_string(), expected_output.to_string())
+    }
 
     #[test]
     fn simple_addition() {        
